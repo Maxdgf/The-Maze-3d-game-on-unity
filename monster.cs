1dfd0d52d521f5fe102755e7bac601a9ecf0645b
Max <162334963+Maxdgf@users.noreply.github.com>
2024-05-29 01:07:47 +0300

    Create monster.cs

---
+++ monster.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Antogonist : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 10f;
+    public float detectionRange = 20f;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer > detectionRange)
+        {
+            agent.SetDestination(RandomPointInNavMesh());
+               
+        }
+        else
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    Vector3 RandomPointInNavMesh()
+    {
+        Vector3 randomDirection = Random.insideUnitCircle * 10f;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, 10f, NavMesh.AllAreas);
+
+        return hit.position;
+    }
+}
+
+
